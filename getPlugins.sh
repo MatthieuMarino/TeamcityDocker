602336bf99f6b249358431ac29e2ad214bd40155
Matthieu <matthieu.marino@gmail.com>
2016-06-06 17:08:59 +0200

    Added script to get plugins

---
+++ getPlugins.sh
@@ -0,0 +1,3 @@
+wget https://teamcity.jetbrains.com/guestAuth/repository/download/bt434/.lastSuccessful/jonnyzzz.node.zip
+wget http://teamcity.jetbrains.com/repository/download/bt402/.lastPinned/deploy-runner.zip?guest=1
+wget https://download.jetbrains.com/teamcity/TeamCity-9.1.7.tar.gz
